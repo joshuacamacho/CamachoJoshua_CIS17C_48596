ed3f70fadd70c8875335c82d7c99951ca0730d4d
joshuacamacho <joshuajcamacho@gmail.com>
2014-10-13 10:46:23 -0700

    Signed-off-by: joshuacamacho <joshuajcamacho@gmail.com>

--- assignments/assignment5/LnkdLst.h
+++ assignments/assignment5/LnkdLst.h
@@ -1,26 +1,54 @@
-/* 
- * File:   LndkdLst.cpp
- * Author: Joshua Camacho
- * Created on October 8, 2014, 9:09 AM
- */
+#ifndef LNKDLST_H
+#define	LNKDLST_H
 
-#include "LndkdLst.h"
-template <class T> 
-LnkdLst::LnkdLst(T x){
-  this->head = new Node;
+#include <string>
+#include <sstream>
+using namespace std;
+template <class T> struct Node{
+         T data;
+         Node *next;
+    };
+template <class T> class LnkdLst {
+private:
+    
+    Node<T> *head;
+    Node<T> *worker;
+
+public:
+    LnkdLst(T);
+    LnkdLst(const LnkdLst&);
+    LnkdLst& operator=(const LnkdLst&);
+    void append(T);
+    void prepend(T);
+    void insertAfter(int,T);
+    void insertBefore(int, T);
+    Node<T>* first();
+    Node<T>* last();
+    void extract(int);
+    string toString();
+    virtual ~LnkdLst();
+    
+
+    
+    
+    
+    
+};
+
+template <class T> LnkdLst<T>::LnkdLst(T x){
+  this->head = new Node<T>;
   this->head->data = x;
   this->head->next = NULL;
   this->worker = this->head;
 }
 
-template <class T>
-LnkdLst::LnkdLst(const LnkdLst& old){
-  Node* temp = old.head;
-  this->head = new Node;
+template <class T> LnkdLst<T>::LnkdLst(const LnkdLst& old){
+  Node<T>* temp = old.head;
+  this->head = new Node<T>;
   this->head->data = old.head->data;
   this->worker = this->head;
   while (temp->next != NULL){
-    this->worker->next = new Node;
+    this->worker->next = new Node<T>;
     this->worker = this->worker->next;
     temp = temp->next;
     this->worker->data = temp->data;
@@ -29,9 +57,8 @@ LnkdLst::LnkdLst(const LnkdLst& old){
   this->worker = this->head;
 }
 
-template <class T>
-LnkdLst& LnkdLst::operator=(const LnkdLst& old){
-  Node* current;
+template <class T> LnkdLst<T>& LnkdLst<T>::operator=(const LnkdLst& old){
+  Node<T>* current;
   worker = head;
   while (worker->next != NULL){
     current = worker;
@@ -42,7 +69,7 @@ LnkdLst& LnkdLst::operator=(const LnkdLst& old){
   worker = head;
   
   while (current->next != NULL){
-    worker->next = new Node;
+    worker->next = new Node<T>;
     worker->data = current->data;
     worker = worker->next;
     current = current->next;
@@ -51,20 +78,18 @@ LnkdLst& LnkdLst::operator=(const LnkdLst& old){
   return *this;
 }
 
-template <class T>
-void LnkdLst::append(T x){
+template <class T> void LnkdLst<T>::append(T x){
   this->worker = head;
   while (this->worker->next != NULL){
     this->worker = this->worker->next;
   }
-  this->worker->next = new Node;
+  this->worker->next = new Node<T>;
   this->worker = this->worker->next;
   this->worker->data = x;
   this->worker->next = NULL;
 }
 
-template <class T>
-string LnkdLst::toString(){
+template <class T> string LnkdLst<T>::toString(){
   stringstream ss;
   this->worker = this->head;
   while (this->worker->next != NULL){
@@ -77,10 +102,10 @@ string LnkdLst::toString(){
 }
 
 template <class T>
-void LnkdLst::prepend(T x){
-  Node *before = this->head;
+void LnkdLst<T>::prepend(T x){
+  Node<T> *before = this->head;
   this->head = NULL;
-  this->head = new Node;
+  this->head = new Node<T>;
   this->head->data = x;
   this->head->next = before;
   before = NULL;
@@ -88,9 +113,9 @@ void LnkdLst::prepend(T x){
 }
 
 template <class T>
-void LnkdLst::insertAfter(int index, T x){
-    Node* temp;
-    Node* newnode = new Node;
+void LnkdLst<T>::insertAfter(int index, T x){
+    Node<T>* temp;
+    Node<T>* newnode = new Node<T>;
     newnode->data = x;
     this->worker = this->head;
     for (int i = 0; i < index+1; i++){
@@ -102,11 +127,11 @@ void LnkdLst::insertAfter(int index, T x){
 }
 
 template <class T>
-void LnkdLst::insertBefore(int index, T x){
+void LnkdLst<T>::insertBefore(int index, T x){
   if (index == 0) prepend(x);
   else{
-    Node* temp;
-    Node* newnode = new Node;
+    Node<T>* temp;
+    Node<T>* newnode = new Node<T>;
     newnode->data = x;
     this->worker = this->head;
     temp = this->worker;
@@ -119,9 +144,8 @@ void LnkdLst::insertBefore(int index, T x){
   }
 }
 
-template <class T>
-LnkdLst::~LnkdLst(){
-  Node* current = head;
+template <class T> LnkdLst<T>::~LnkdLst(){
+  Node<T>* current = head;
   while (current != NULL){
     this->worker = current;
     current = current->next;
@@ -132,13 +156,13 @@ LnkdLst::~LnkdLst(){
 }
 
 template <class T>
-LnkdLst::Node* LnkdLst::first(){
+Node<T>* LnkdLst<T>::first(){
   return this->head;
 }
 
 template <class T>
-LnkdLst::Node* LnkdLst::last(){
-  Node* last = this->head;
+Node<T>* LnkdLst<T>::last(){
+  Node<T>* last = this->head;
   while (last->next!=NULL){
    last = last->next;
   }
@@ -146,8 +170,8 @@ LnkdLst::Node* LnkdLst::last(){
 }
 
 template <class T>
-void LnkdLst::extract(int index){
-  Node* temp;
+void LnkdLst<T>::extract(int index){
+  Node<T>* temp;
   if (index > 1){
     temp = this->head;
     for (int i = 0; i < index; i++){
@@ -170,3 +194,5 @@ void LnkdLst::extract(int index){
     delete temp;
   }
 }
+#endif	/* LNKDLST_H */
+
--- assignments/assignment5/main.cpp
+++ assignments/assignment5/main.cpp
@@ -5,12 +5,22 @@
 
 #include <cstdlib>
 #include <iostream>
-#include "LndkdLst.h"
+#include "LnkdLst.h"
 
 
 
 int main(int argc, char** argv) {
+    LnkdLst<char> * list = new LnkdLst<char>('2');
+    list->append('t');
+    list->prepend('3');
+    list->insertAfter(1,'r');
+    list->insertBefore(0,'f');
     
+    LnkdLst<char>* copy = new LnkdLst<char>('3');
+    
+    copy=list;
+    cout<<list->toString()<<"\n";
+    cout<<copy->toString();
     return 0;
 }
 
